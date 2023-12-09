1fa8439789a672966557d29ec5f0f82ed3aadeac
Junaid Kamal <149483027+junaidkamal@users.noreply.github.com>
2023-12-09 10:08:43 +0500

    Add files via upload

---
+++ JavaScript Assignment #12/script.js
@@ -0,0 +1,130 @@
+
+    1     //       Aler Example    //
+
+    //      alert("Zaid");
+
+    2    //    Variables For Input String Example    //
+
+
+  /*   var myName = "Thanks for Warm Wishes";
+     alert(myName);   */
+
+    3 & 4   // Variables For Numbers Example   //
+
+
+  /*   var firstNum = 23;
+     var secondNum = 23;
+
+     var originalNum = firstNum + secondNum;
+     alert(originalNum);  */
+
+   5 ,6 & 7  //  Math Expressions.....  //
+
+  /*    var bestNum = 6+6;
+      alert(bestNum);  */
+
+
+    /*  var bestNum = 9 * 2;
+      alert(bestNum);  */
+
+    /*  var bestNum = (1 + 2) * 4;
+      alert(bestNum);  */
+
+      8 // Concatenating Text String //
+
+
+     /*  var userName = "Junaid";
+      var msg = "Thanks";
+      var sign = "!";
+      var result =  userName + msg + sign;
+      alert(result);   */
+
+
+      9 // Prompts //
+
+  /*    var firstName = "Junaid";
+      var lastName = "Kamal";
+      var result = prompt(firstName + lastName);  */
+
+      10 // if Statement //
+
+    /*   var x = prompt("Who is the ceo of yt?");
+ if (x === "Neal Mohan") {
+ alert("Neal Mohan");
+}   */
+
+
+   11 // Comparision Operators  //
+
+  /*   if (yourName !== Asad) {
+ alert("You are not able");
+}  */
+
+12 // If else and else if //
+
+/*  var x = "Vatican";
+if (x === "Vatican") {
+alert("Correct!");
+ }
+ else {
+ alert("Wrong answer");
+} */
+
+
+ 13  & 14//               //
+
+/* var age = 20;
+if ( age == 20) {
+  alert("Congrats");
+}  */
+
+
+15 ,16 ,17 // Arrays //
+/*
+var a ="A";
+var b = "B";
+var arr = ["A" ,"B" ,"C" ,"D" ,"E" ,"F"]
+console.log(arr[4]); */
+
+/* var a ="A";
+var b = "B";
+var arr = ["A" ,"B" ,"C" ,"D" ,"E" ,"F"]
+arr[3] = 20;
+console.log(arr); */
+
+/* var a ="A";
+var b = "B";
+var arr = ["A" ,"B" ,"C" ,"D" ,"E" ,"F"]
+arr.push("D");
+console.log(arr); */
+
+/* var a ="A";
+var b = "B";
+var arr = ["A" ,"B" ,"C" ,"D" ,"E" ,"F"]
+arr.pop("E");
+console.log(arr); */
+
+/* var a ="A";
+var b = "B";
+var arr = ["A" ,"B" ,"C" ,"D" ,"E" ,"F"]
+arr.shift("C");
+console.log(arr); */
+
+/* var a ="A";
+var b = "B";
+var arr = ["A" ,"B" ,"C" ,"D" ,"E" ,"F"]
+arr.unshift("DEC");
+console.log(arr); */
+
+
+/* var a ="A";
+var b = "B";
+var arr = ["A" ,"B" ,"C" ,"D" ,"E" ,"F"]
+arr.slice(3);
+console.log(arr); */
+
+var a ="A";
+var b = "B";
+var arr = ["A" ,"B" ,"C" ,"D" ,"E" ,"F"]
+arr.splice(1,4);
+console.log(arr);
